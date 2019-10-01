61efefaa91bb4990b1288e05ece28442f4295874
Vitaliy Prikota <mr.prikota@gmail.com>
2019-10-01 21:37:56 +0300

    Spliced tokenizer functionality to different methods, added simple validation.

--- src/RPNSolver.java
+++ src/RPNSolver.java
@@ -1,10 +1,10 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class RPNSolver {
     public static int solve(String[] tokens) throws Exception {
         Deque<String> stack = new LinkedList<>();
+        System.out.println(Arrays.toString(tokens));
         for (String token : tokens) {
             if (Pattern.matches("-?[0-9]{1,10}", token)) {
                 stack.push(token);
@@ -33,10 +33,10 @@ public class RPNSolver {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(solve(Tokenizer.postfix("-2 * ( 3 + 5 )")));
-        System.out.println(solve(Tokenizer.postfix("2 / ( 3 + 5 )")));
-        System.out.println(solve(Tokenizer.postfix("2 / 3 + 5 ")));
-        System.out.println(solve(Tokenizer.postfix("-50 * ( 30 + ( 40 - 20 ) / 2 )")));
+        Map<String, String> vars = new HashMap<>();
+        vars.put("a", "30");
+        System.out.println(RPNSolver.solve(Tokenizer.tokenize("2 * (30 + a) / 100 * a", vars)));
+        System.out.println(RPNSolver.solve(Tokenizer.tokenize("3 + 4 * 2 / (1 - 5)", vars)));
     }
 }
 
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tokenizer {
 
@@ -11,7 +13,7 @@ public class Tokenizer {
         }
     }
 
-    public static Map<String, Operator> ops = new HashMap<>() {{
+    static Map<String, Operator> ops = new HashMap<>() {{
         put("+", Operator.ADD);
         put("-", Operator.SUBTRACT);
         put("*", Operator.MULTIPLY);
@@ -22,11 +24,11 @@ public class Tokenizer {
         return (ops.containsKey(sub) && ops.get(sub).precedence >= ops.get(op).precedence);
     }
 
-    public static String[] postfix(String infix) {
+    private static String[] buildPostfix(ArrayList<String> tokens) {
         StringBuilder output = new StringBuilder();
         Deque<String> stack = new LinkedList<>();
 
-        for (String token : infix.split("\\s")) {
+        for (String token : tokens) {
             if (ops.containsKey(token)) {
                 while (!stack.isEmpty() && isHigerPrec(token, stack.peek()))
                     output.append(stack.pop()).append(' ');
@@ -48,12 +50,73 @@ public class Tokenizer {
         return output.toString().split(" ");
     }
 
+    private static ArrayList<String> buildTokensList(String expr) {
+        expr = expr.replaceAll("\\s+", "");
+        String[] literals = expr.split("");
+//        System.out.println("Literals: " + Arrays.toString(literals));
+        StringBuilder numberBuffer = new StringBuilder();
+        ArrayList<String> tokens = new ArrayList<>();
+
+        for (int i = 0; i < literals.length; i++) {
+            if (i == 0 && literals[i].equals("-")) {
+                numberBuffer.append("-");
+            }
+            else if (literals[i].equals("-") && Pattern.matches("[+*/()]", literals[i-1])) {
+                numberBuffer.append("-");
+            }
+            else if (Pattern.matches("[0-9]", literals[i])) {
+                numberBuffer.append(literals[i]);
+            }
+            else if (Pattern.matches("[-+*/()]", literals[i])) {
+                if (numberBuffer.length() > 0) {
+                    tokens.add(numberBuffer.toString());
+                    numberBuffer.delete(0, numberBuffer.length());
+                }
+                tokens.add(literals[i]);
+            }
+            else if (Pattern.matches("[a-z]", literals[i])) {
+                tokens.add(literals[i]);
+            }
+            if (i == literals.length - 1) {
+                if (numberBuffer.length() > 0) {
+                    tokens.add(numberBuffer.toString());
+                    numberBuffer.delete(0, numberBuffer.length());
+                }
+            }
+        }
+
+        return tokens;
+    }
+
+    private static String replaceVars(Map<String, String> varsMap, String expr) {
+        for (Map.Entry<String, String> var : varsMap.entrySet()) {
+            expr = expr.replaceAll(var.getKey(), var.getValue());
+        }
+        return expr;
+    }
+
+    static String[] tokenize(String expr, Map<String, String> varsMap) {
+        validate(expr);
+        expr = replaceVars(varsMap, expr);
+        return buildPostfix(buildTokensList(expr));
+    }
+
+    private static void validate(String expr) {
+        Matcher matcher = Pattern.compile("[^-+/*a-z0-9\\s()]").matcher(expr);
+        if (matcher.find()) {
+            throw new Error("That expression contains inappropriate symbols: " + matcher.group());
+        }
+    }
+
     public static void main(String[] args) {
         test();
     }
 
-    public static void test() {
-        System.out.println(Arrays.toString(postfix("2 * ( 3 - 2 )")));
-        System.out.println(Arrays.toString(postfix("- 5 * ( 3 - 2 )")));
+    private static void test() {
+        Map<String, String> vars = new HashMap<>();
+        vars.put("a", "30");
+        System.out.println(Arrays.toString(Tokenizer.tokenize("2 * (30 + a) /    120", vars)));
+        System.out.println(Arrays.toString(Tokenizer.tokenize("-5 * (3 - 2)", vars)));
+        System.out.println(Arrays.toString(Tokenizer.tokenize("-5 * (3 - 2) + (-10*20) + a", vars)));
     }
 }
